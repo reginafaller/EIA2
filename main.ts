8c066694875c208810f23329dfc7e3379bbf7180
regina faller <reginafaller@gmx.net>
2019-03-21 15:30:45 +0100

    update

---
+++ main.ts
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", greeter);
+
+function greeter():void {
+    var promptValue = prompt('name eingeben');
+    console.log('herzlich willkommen', promptValue, 'ich habe sie erwartet');
+
+
+    document.getElementById('name').innerHTML = ('herzlich willkommen ' + promptValue + ' ich habe sie erwartet');
+}
+
