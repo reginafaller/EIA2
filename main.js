568f0671df956f7ed8213ea16b8c563f3be3c1f8
regina faller <reginafaller@gmx.net>
2019-03-28 15:05:19 +0100

    aufgabe2

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", greeter);
-function greeter() {
-    var promptValue = prompt('name eingeben');
-    console.log('herzlich willkommen', promptValue, 'ich habe sie erwartet');
-    document.getElementById('name').innerHTML = ('herzlich willkommen ' + promptValue + ' ich habe sie erwartet');
-}
-//# sourceMappingURL=main.js.map
