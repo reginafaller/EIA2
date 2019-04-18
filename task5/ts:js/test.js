209a7a97c78f3496a5acb5cb284641acf0a4174b
regina faller <reginafaller@gmx.net>
2019-04-18 17:08:59 +0200

    Aufgabe 4

--- task5/ts:js/test.js
+++ task5/ts:js/test.js
@@ -1,23 +1,63 @@
 var L04_FormElements;
 (function (L04_FormElements) {
     window.addEventListener("load", init);
+    document.addEventListener("DOMContentLoaded", button);
     function init(_event) {
-        console.log("Init");
         let fieldsets = document.getElementsByTagName("fieldset");
         for (let i = 0; i < fieldsets.length; i++) {
             let fieldset = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
         }
     }
+    let n = 0;
     function handleChange(_event) {
-        console.log(_event);
-        let eissorte = _event.target;
-        let preisEissorte = parseFloat(eissorte.value);
-        berechnePreis(preisEissorte);
+        berechnePreis(_event);
     }
-    function berechnePreis(_preis) {
-        let n = 0;
-        console.log(n + _preis);
+    function button() {
+        let button = document.getElementById("bestellen");
+        button.addEventListener("click", validate);
+    }
+    function validate(_event) {
+        let fehltArray = [];
+        let a = 0;
+        let validate = document.getElementsByTagName("input");
+        for (let i = 0; i < validate.length; i++) {
+            if (validate[i].value == "") {
+                let feldName = validate[i].name;
+                fehltArray.push(feldName);
+            }
+        }
+        if (fehltArray.length == 0) {
+            alert("danke");
+        }
+        else {
+            alert(`bitte ${fehltArray} ausfullen`);
+        }
+    }
+    function berechnePreis(_event) {
+        n = 0;
+        document.getElementById("Bestellung").innerHTML = '';
+        let input = document.getElementsByTagName("input");
+        for (let i = 0; i < input.length; i++) {
+            if (input[i].checked == true) {
+                let preis = Number(input[i].value);
+                n += preis;
+                let bezeichung = document.createElement("li");
+                bezeichung.innerHTML = `<p>${input[i].className}</p>`;
+                document.getElementById("Bestellung").appendChild(bezeichung);
+            }
+            if (input[i].name == "sauce") {
+                let stellungSlider = Number(input[i].value);
+                let preisSlider = Number(input[i].id);
+                n += stellungSlider * preisSlider;
+                if (stellungSlider > 0) {
+                    let bezeichung = document.createElement("li");
+                    bezeichung.innerHTML = `<p>${stellungSlider} ${input[i].className}</p>`;
+                    document.getElementById("Bestellung").appendChild(bezeichung);
+                }
+            }
+        }
+        document.getElementById("price").innerHTML = n.toFixed(2).toString();
     }
 })(L04_FormElements || (L04_FormElements = {}));
 //# sourceMappingURL=test.js.map
--- task5/ts:js/test.ts
+++ task5/ts:js/test.ts
@@ -1,8 +1,8 @@
 namespace L04_FormElements {
     window.addEventListener("load", init);
+    document.addEventListener("DOMContentLoaded", button)
 
     function init(_event: Event): void {
-        console.log("Init");
         let fieldsets: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
 
         for (let i: number = 0; i < fieldsets.length; i++) {
@@ -11,15 +11,54 @@ namespace L04_FormElements {
         }
     }
 
+    let n: number = 0;
+
     function handleChange(_event: Event): void {
-        console.log(_event)
-        let eissorte: HTMLInputElement = <HTMLInputElement>_event.target;
-        let preisEissorte = parseFloat(eissorte.value);
-        berechnePreis(preisEissorte)
+        berechnePreis(_event)
     }
 
-    function berechnePreis(_preis:number){
-        let n:number= 0;
-        console.log(n+_preis);
+    function button() {
+        let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("bestellen");
+        button.addEventListener("click", validate)
     }
+
+    function validate(_event: Event): void {
+        let fehltArray: string[] = [];
+        let a: number = 0;
+        let validate: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+        for (let i: number = 0; i < validate.length; i++) {
+            if (validate[i].value == "") {
+                let feldName: string = validate[i].name
+                fehltArray.push(feldName)
+            }
+        } if (fehltArray.length == 0) {
+            alert("danke");
+        } else { alert(`bitte ${fehltArray} ausfullen`); }
     }
+
+    function berechnePreis(_event: Event): void {
+        n = 0;
+        document.getElementById("Bestellung").innerHTML = '';
+        let input: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
+        for (let i: number = 0; i < input.length; i++) {
+            if (input[i].checked == true) {
+                let preis: number = Number(input[i].value)
+                n += preis;
+                let bezeichung = document.createElement("li");
+                bezeichung.innerHTML = `<p>${input[i].className}</p>`
+                document.getElementById("Bestellung").appendChild(bezeichung)
+
+            }
+            if (input[i].name == "sauce") {
+                let stellungSlider: number = Number(input[i].value)
+                let preisSlider: number = Number(input[i].id)
+                n += stellungSlider * preisSlider;
+                if (stellungSlider > 0) {
+                    let bezeichung = document.createElement("li");
+                    bezeichung.innerHTML = `<p>${stellungSlider} ${input[i].className}</p>`
+                    document.getElementById("Bestellung").appendChild(bezeichung)
+                }
+            }
+        } document.getElementById("price").innerHTML = n.toFixed(2).toString();
+    }
+}
