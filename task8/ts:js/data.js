f2ba25cfcbf4ef7432dafdd84d69c5a1bd0318e6
regina faller <reginafaller@gmx.net>
2019-05-09 20:30:06 +0200

    radioButton labels

--- task8/ts:js/data.js
+++ task8/ts:js/data.js
@@ -10,12 +10,12 @@ var L04_AssocArraysAndExport;
         ],
         "sahne": [
             { type: "radio", name: "sahne", value: 0, id: "radio", class: "Keine Sahne", step: 0, min: 0, max: 0, price: 0 },
-            { type: "radio", name: "sahne", value: 0, id: "radio", class: "Laktosefreie Sahne", step: 0, min: 0, max: 0, price: 1 },
-            { type: "radio", name: "sahne", value: 0, id: "radio", class: "Sahne", step: 0, min: 0, max: 0, price: 1.2 }
+            { type: "radio", name: "sahne", value: 0, id: "radio1", class: "Laktosefreie Sahne", step: 0, min: 0, max: 0, price: 1 },
+            { type: "radio", name: "sahne", value: 0, id: "radio2", class: "Sahne", step: 0, min: 0, max: 0, price: 1.2 }
         ],
         "Anrichtung": [
-            { type: "radio", name: "anricht", value: 0, id: "radio2", class: "In Waffel", step: 0, min: 0, max: 0, price: 1.5 },
-            { type: "radio", name: "anricht", value: 0, id: "radio2", class: "Im Becher", step: 0, min: 0, max: 0, price: 1 },
+            { type: "radio", name: "anricht", value: 0, id: "radio3", class: "In Waffel", step: 0, min: 0, max: 0, price: 1.5 },
+            { type: "radio", name: "anricht", value: 0, id: "radio4", class: "Im Becher", step: 0, min: 0, max: 0, price: 1 },
         ],
         "Extras": [
             { type: "checkbox", name: "chocolate chunks", value: 0, id: "check6", class: "chocolate chunks", step: 0, min: 0, max: 0, price: 1 },
